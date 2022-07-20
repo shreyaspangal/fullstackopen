696a5be4166367addcdc52f086a03345e92d48b9
shreyaspangal <shreyaspangal>
2022-07-20 23:44:59 +0530

    Refactor & debug app & index files

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,34 +1,47 @@
-import Header from './Header';
-import Content from './Content';
-import Total from './Total';
+import { useState } from "react";
+import Display from "./Display";
+import Button from "./Button";
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
-
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      button press history: {props.allClicks.join(" ")}
     </div>
-
   )
 }
-export default App
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/courseinfo/src/Display.js
@@ -0,0 +1,7 @@
+const Display = ({ counter }) => {
+    return (
+        <div>Counter: {counter}</div>
+    )
+}
+
+export default Display;
