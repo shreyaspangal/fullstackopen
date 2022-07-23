cc620eb3f67131ba1b3e30b4ff38022425d1027c
shreyaspangal <shreyaspangal>
2022-07-23 14:16:06 +0530

    Courseinfo excercise

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,6 +5,6 @@ import AnecdotesApp from './anecdotes/App'
 import CourseinfoApp from './courseinfo/App'
 import UnicafeApp from './unicafe/App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<AnecdotesApp />);
+// ReactDOM.createRoot(document.getElementById('root')).render(<AnecdotesApp />);
 // ReactDOM.createRoot(document.getElementById('root')).render(<CourseinfoApp />);
-// ReactDOM.createRoot(document.getElementById('root')).render(<UnicafeApp />);
+ReactDOM.createRoot(document.getElementById('root')).render(<UnicafeApp />);
