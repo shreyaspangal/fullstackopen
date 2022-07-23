d0267cbfd355292905f2f61a2dc2cc943f1d5350
shreyaspangal <shreyaspangal>
2022-07-23 12:17:27 +0530

    Refactor & modularize file structure

---
+++ part1/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import AnecdotesApp from './anecdotes/App'
+import CourseinfoApp from './courseinfo/App'
+import UnicafeApp from './unicafe/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<AnecdotesApp />);
+// ReactDOM.createRoot(document.getElementById('root')).render(<CourseinfoApp />);
+// ReactDOM.createRoot(document.getElementById('root')).render(<UnicafeApp />);
