6fd3e043c4087907648bab93f15781d3a010f484
shreyaspangal <shreyaspangal>
2022-07-25 23:59:33 +0530

    Refactor Ex:2.1 to 2.5 into courseinfo 7 components directory

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import CourseinfoApp from './courseinfo/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CourseinfoApp />
   </React.StrictMode>
 );
