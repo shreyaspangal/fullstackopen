aa1209d04d7109daeeba73f3257ffd73e45cb38f
shreyaspangal <shreyaspangal>
2022-07-28 12:27:47 +0530

    Ex 2.9 - Phonebook
    - add filter functionality as user types in every character

--- part2/src/phonebook/App.js
+++ part2/src/phonebook/App.js
@@ -1,11 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [filter, setFilter] = useState("");
 
+    //Common variable for holding person data to display
+    const contacsToShow = !filter ? persons : persons.filter(person => {
+        return Object.values(person).join('').toLowerCase().includes(filter.toLowerCase());
+    });
+
+    // Helper function for handling common onchange events
+    const handleOnChange = () => (event, setFunc) => {
+        setFunc(event.target.value)
+    }
+    // Custom onChange handler
+    const onChange = handleOnChange();
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -15,24 +31,32 @@ const App = () => {
             alert(`${newName} is already added to phonebook`);
             return;
         }
-        // If not found update the contacts list
-        setPersons(persons.concat({ name: newName }));
+        // If new contact, then update the contacts list
+        setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length + 1 }));
         setNewName('');
+        setNewNumber('');
     }
 
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                Filter: <input type="text" value={filter} onChange={(e) => onChange(e, setFilter)} />
+            </div>
             <form onSubmit={handleSubmit}>
+                <h3>Add new contact</h3>
                 <div>
-                    name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+                    name: <input type="text" value={newName} onChange={(e) => onChange(e, setNewName)} />
+                </div>
+                <div>
+                    number: <input type="number" value={newNumber} onChange={(e) => onChange(e, setNewNumber)} />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+            {!contacsToShow.length ? 'No match found! - try again with another name.' : contacsToShow.map((person) => <div key={person.id}>{person.name}{" "}{person.number}</div>)}
         </div>
     )
 }
