24f8b98f5378a4e2832d0355963942b1a70c8fe0
Shreyas Pangal <shreyaspangal03@gmail.com>
2022-08-18 23:44:20 +0530

    Ex: 3.1 - Phonebook - hardcoded list of phonebook entries

--- part3/index.js
+++ part3/index.js
@@ -1,30 +1,34 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-let notes = [
+app.use(express.json());
+
+let persons = [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ];
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end(JSON.stringify(notes))
-});
+app.get('/api/persons', (req, res) => {
+    res.json(persons).status(200);
+})
 
 const PORT = 3001;
 
