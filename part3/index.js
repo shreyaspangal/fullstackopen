f3bd8eb11d3ec1982cf3c6629604c0a5b52c16c3
Shreyas Pangal <shreyaspangal03@gmail.com>
2022-08-20 12:52:38 +0530

    Minor Fixes

--- part3/index.js
+++ part3/index.js
@@ -3,11 +3,11 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :body'));
-app.use(express.static('build'))
 
 let persons = [
     {
